a760de3c5fc0a40ee560c4efc7409bddfc296e2a
Lucas Lima <lucasflima25@gmail.com>
2020-12-14 21:56:29 -0300

    Adicionado meta tags

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,12 @@ const Index = () => {
             <title>Lucas Lima</title>
             <link rel="shortcut icon" href="/favicon.ico" />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta content="My personal site." name="description"/>
+
+            <meta property="og:title" content="Lucas Lima"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:description" content="My personal site."/>
+            <meta property="og:image"content="https://lukeflima.dev.br/favicon.ico"/>
         </Head>
         <Navbar title={title}/>
 
