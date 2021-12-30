1af11b01ce60bc1843a7bf714cc2f5e30c09f11d
Lucas Lima <lukeflima@gmail.com>
2021-12-30 04:52:49 -0300

    Try fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-    webpack(config) {
-        config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
+    webpack(config, { isServer, dev }) {
+        // Enable webassembly
+        config.experiments = { asyncWebAssembly: true };
 
-        // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-        config.experiments = { asyncWebAssembly: true }
+        // In prod mode and in the server bundle (the place where this "chunks" bug
+        // appears), use the client static directory for the same .wasm bundle
+        config.output.webassemblyModuleFilename =
+            isServer && !dev ? "../static/wasm/[id].wasm" : "static/wasm/[id].wasm";
 
-        return config
+        // Ensure the filename for the .wasm bundle is the same on both the client
+        // and the server (as in any other mode the ID's won't match)
+        config.optimization.moduleIds = "named";
+
+        return config;
     },
-}
+};
--- public/qoi_viewer/qoi_viewer.js
+++
@@ -1,2 +0,0 @@
-import * as wasm from "./qoi_viewer_bg.wasm";
-export * from "./qoi_viewer_bg.js";
